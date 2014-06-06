fa30c21b6f36db61bef20ff1447f15533fe87215
jefrailey <jcaetof@gmail.com>
2014-06-06 12:02:09 -0700

    Update test functions with shorter error message and cleaned up formatting

--- parenthetics/test_proper_parenthetics.py
+++ parenthetics/test_proper_parenthetics.py
@@ -1,41 +1,38 @@
-import pytest
-import proper_parenthetics
+import proper_parenthetics as pp
+
+answer = u"All the right pieces in all the wrong places."
 
 
 def test_open():
     a_string = u"(hello( my good friend)"
-    assert proper_parenthetics.parentheticals(a_string) == -1
+    assert pp.parentheticals(a_string) == -1
 
 
 def test_closed():
     a_string = u"(hello( my good friend)))"
-    assert proper_parenthetics.parentheticals(a_string) == 1
+    assert pp.parentheticals(a_string) == 1
 
 
 def test_balanced_proper():
     a_string = u"(hello( my good friend))"
-    assert proper_parenthetics.parentheticals(a_string) == 0
+    assert pp.parentheticals(a_string) == 0
 
 
 def test_balanced_improper1():
     a_string = u")helo (hello( my good friend)"
-    answer = u"You've got all the right pieces in all the wrong places."
-    assert proper_parenthetics.parentheticals(a_string) == answer
+    assert pp.parentheticals(a_string) == answer
 
 
 def test_balanced_improper2():
     a_string = u")()()()helo (hello( my go()()()()od friend)"
-    answer = u"You've got all the right pieces in all the wrong places."
-    assert proper_parenthetics.parentheticals(a_string) == answer
+    assert pp.parentheticals(a_string) == answer
 
 
 def test_balanced_improper3():
     a_string = u")helo (hello( my good friend)()()()()"
-    answer = u"You've got all the right pieces in all the wrong places."
-    assert proper_parenthetics.parentheticals(a_string) == answer
+    assert pp.parentheticals(a_string) == answer
 
 
 def test_balanced_improper4():
     a_string = u"())(((((((((())))))))))("
-    answer = u"You've got all the right pieces in all the wrong places."
-    assert proper_parenthetics.parentheticals(a_string) == answer
+    assert pp.parentheticals(a_string) == answer
