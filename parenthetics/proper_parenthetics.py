643dd66c45352bdbe150767929d6abd766b6034a
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-09 09:44:38 -0700

    Update proper_parenthetics.py
    Created more reasonable output for outlying situation.

--- parenthetics/proper_parenthetics.py
+++ parenthetics/proper_parenthetics.py
@@ -2,14 +2,14 @@ def parentheticals(a_string):
     _close = a_string.count(u")")
     _open = a_string.count(u"(")
     if _open > _close:
-        return -1
-    elif _open < _close:
         return 1
+    elif _open < _close:
+        return -1
     elif _open == _close:
         _count = 0
         for letter in a_string:
             if _count > 0:
-                return u"All the right pieces in all the wrong places."
+                return -2
             elif letter == u"(":
                 _count -= 1
             elif letter == u")":
