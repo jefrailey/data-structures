8a8ba8abc05cee5dddce4469f52c6a53ba724742
jefrailey <jcaetof@gmail.com>
2014-06-09 07:56:53 -0700

    Add test_queue.py with skeleton test functions for queue.py

---
+++ queue/test_queue.py
@@ -0,0 +1,27 @@
+import queue
+
+
+def _make_queue():
+    pass
+
+
+def test_init_queue():
+    pass
+
+
+def tests_init_node():
+    pass
+
+
+def test_enqueue():
+    pass
+
+
+def test_dequeue():
+    # Need to test dequeue() on an empty queue, queue of 1 value, and a queue
+    # with multiple values
+    pass
+
+
+def test_size():
+    pass
