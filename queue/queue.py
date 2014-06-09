e232f6ec17c90af7ad1f9ac0e8837282ebda4e60
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-09 14:08:29 -0700

    Wrote Queue class, inheriting from the linked_list. Fleshed out the tests.

--- queue/queue.py
+++ queue/queue.py
@@ -1,10 +1,23 @@
-class Queue(object):
-    """docstring for Queue"""
+from linked_list import LinkedList
+
+
+class Queue(LinkedList):
+    u"""Creates a queue object that holds values in a FiFo format."""
     def __init__(self):
         super(Queue, self).__init__()
 
+    def enqueue(self, value):
+        u"""Adds a value/node to the head of the Queue object."""
+        self.insert(value)
 
-class Node(object):
-    """docstring for Node"""
-    def __init__(self):
-        super(Node, self).__init__()
+    def dequeue(self):
+        u"""Removes a value/node from the tail of the Queue object."""
+        if self.head_node:
+            node = self.head_node
+            while node.the_next:
+                node = node.the_next
+            else:
+                last_node = node
+            self.remove(last_node)
+        else:
+            raise LookupError
--- queue/test_queue.py
+++ queue/test_queue.py
@@ -1,38 +1,39 @@
-from queue import Queue, Node
+from queue import Queue
+import pytest
 
 value_tuple = (u'a', 1, True, None, False, 0)
 
 
 def test_init_queue():
+    u"""Test Queue init."""
     q = Queue()
     assert isinstance(q, Queue)
 
 
-def tests_init_node():
-    for val in value_tuple:
-        n = Node(val)
-        assert isinstance(n, Node)
-
-
 def test_enqueue():
+    u"""Asserts nodes are added and hold their own value."""
     q = Queue()
     for val in value_tuple:
         q.enqueue(val)
-        assert q.head.value == value_tuple[0]
-        assert q.tail.value == val
+        assert q.head_node.value == val
 
 
 def test_dequeue():
+    u"""Asserts nodes are properly removed."""
     q = Queue()
     for val in value_tuple:
         q.enqueue(val)
     for val in value_tuple:
         q.dequeue()
-        assert q.head.value == val
-        assert q.tail.value == value_tuple[-1]
+        if q.head_node:
+            assert q.head_node.value == value_tuple[-1]
+        else:
+            with pytest.raises(LookupError):
+                q.dequeue()
 
 
 def test_size():
+    u"""Asserts size of Queue remains as expected during enqueue process."""
     q = Queue()
     for i, val in enumerate(value_tuple, 1):
         q.enqueue(val)
