e67093289370a64415d5adb98f42893db5bfbd57
jefrailey <jcaetof@gmail.com>
2014-06-09 12:30:08 -0700

    Add queue.py with Queue() and Node() skeletons.

---
+++ queue/queue.py
@@ -0,0 +1,10 @@
+class Queue(object):
+    """docstring for Queue"""
+    def __init__(self):
+        super(Queue, self).__init__()
+
+
+class Node(object):
+    """docstring for Node"""
+    def __init__(self):
+        super(Node, self).__init__()
