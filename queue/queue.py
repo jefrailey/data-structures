6c5f58dfe51eacb6a3b211004dd7eeae6a759a8d
jefrailey <jcaetof@gmail.com>
2014-06-09 19:03:46 -0700

    Refactor Queue() to contain LinkedList() instead of sublcassing it to place the user one step further form unneeded methods. Added __str__() to Queue() to print nicely

--- queue/queue.py
+++ queue/queue.py
@@ -1,25 +1,41 @@
 from linked_list import LinkedList
 
 
-class Queue(LinkedList):
+class Queue(object):
     u"""Creates a queue object that holds values in a FiFo format."""
     def __init__(self):
-        super(Queue, self).__init__()
+        self.list = LinkedList()
 
     def enqueue(self, value):
         u"""Adds a value/node to the head of the Queue object."""
-        self.insert(value)
+        self.list.insert(value)
 
     def dequeue(self):
         u"""Removes a value/node from the tail of the Queue object."""
-        if self.head_node:
-            node = self.head_node
+        if self.list.head_node:
+            node = self.list.head_node
             while node.the_next:
                 node = node.the_next
             else:
                 last_node = node
             a = last_node.value
-            self.remove(last_node)
+            self.list.remove(last_node)
             return a
         else:
             raise LookupError
+
+    def size(self):
+        return self.list.size()
+
+    def __str__(self):
+        q_str = u")"
+        if self.list.head_node:
+            node = self.list.head_node
+            while node.the_next:
+                if len(q_str) == 1:
+                    q_str = u"{}".format(node.value) + q_str
+                else:
+                    q_str = u"{},".format(node.value) + q_str
+                node = node.the_next
+            q_str = u"{},".format(node.value) + q_str
+        return u"({}".format(q_str)
