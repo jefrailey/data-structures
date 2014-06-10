3b64b1fea39c644078e3a0d34309b73c64f267e9
jefrailey <jcaetof@gmail.com>
2014-06-10 07:32:24 -0700

    Add doubly_linked.py with skeletons for expected classes.

---
+++ doubly_linked/doubly_linked.py
@@ -0,0 +1,12 @@
+class DoublyLinked(object):
+    """docstring for DoublyLinked"""
+    def __init__(self, arg):
+        super(DoublyLinked, self).__init__()
+        self.arg = arg
+
+
+class Node(object):
+    """docstring for Node"""
+    def __init__(self, arg):
+        super(Node, self).__init__()
+        self.arg = arg
