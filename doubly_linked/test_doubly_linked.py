21867c9a587bff868248c05eb8d6af66878fdf59
jefrailey <jcaetof@gmail.com>
2014-06-10 07:30:06 -0700

    Add test_doubly_linked.py with skeleton tests for instantiating expected classes and testing methods.

---
+++ doubly_linked/test_doubly_linked.py
@@ -0,0 +1,38 @@
+from doubly_linked import DoublyLinked, Node
+
+value_tuple = (u'a', 1, True, None, False, 0)
+
+
+def test_Node_init():
+    u"""Assert a Node instantiates with a given value."""
+    pass
+
+
+def test_DoublyLinked_init():
+    u"""Assert a DoublyLinked list instantiates with a given values."""
+    pass
+
+
+def test_insert():
+    u"""Assert .insert() adds a value to the head of the doubly linked list."""
+    pass
+
+
+def test_append():
+    u"""Assert .append() adds a value to the tail of the doubly linked list."""
+    pass
+
+
+def test_pop():
+    u"""Assert .pop() removes and returns the head value from the list."""
+    pass
+
+
+def test_shift():
+    u"""Assert .shift() removes and returns the tail value from the list."""
+    pass
+
+
+def test_remove():
+    u"""Assert .remove() removes the first matching value from the list."""
+    pass
