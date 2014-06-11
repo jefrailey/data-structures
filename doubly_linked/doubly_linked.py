ccfbef63dc4478717d00c254c221e451b3370795
jefrailey <jcaetof@gmail.com>
2014-06-10 21:49:36 -0700

    Improve formatting and docstrings.

--- doubly_linked/doubly_linked.py
+++ doubly_linked/doubly_linked.py
@@ -1,17 +1,19 @@
 class DoublyLinked(object):
-    """docstring for DoublyLinked"""
+    """Create an object that represents a doubly linked list."""
     def __init__(self, head=None, tail=None):
         self.h = head
         self.t = tail
 
-    def insert(self,value):
+    def insert(self, value):
+        u"""Insert a value at the head of the doubly linked list."""
         node = Node(value)
         if not self.h:
             self.h = self.t = node
         elif self.h:
             node.twards, self.h.hwards, self.h = self.h, node, node
 
-    def append(self,value):
+    def append(self, value):
+        u"""Append a value to the tail of the doubly linked list."""
         node = Node(value)
         if not self.h:
             self.h = self.t = node
@@ -22,7 +24,11 @@ class DoublyLinked(object):
         u"""Removes and returns the h value from the list."""
         returnValue = None
         try:
-            returnValue, self.h, self.h.hwards = self.h.value, self.h.twards, None
+            returnValue, self.h, self.h.hwards = (
+                self.h.value,
+                self.h.twards,
+                None
+                )
         except AttributeError:
             returnValue, self.h, self.t = self.h.value, None, None
         finally:
@@ -32,7 +38,11 @@ class DoublyLinked(object):
         u"""Removes and returns the t value from the list."""
         returnValue = None
         try:
-            returnValue, self.t, self.t.twards = self.t.value, self.t.hwards, None
+            returnValue, self.t, self.t.twards = (
+                self.t.value,
+                self.t.hwards,
+                None
+                )
         except AttributeError:
             returnValue, self.t, self.h = self.t.value, None, None
         finally:
@@ -40,7 +50,7 @@ class DoublyLinked(object):
 
     def remove(self, value):
         u"""Removes the first matching value from the list."""
-        if self.h == None:
+        if self.h is None:
             raise LookupError
         elif self.h is self.t and self.h.value == value:
             self.h = None
@@ -57,16 +67,14 @@ class DoublyLinked(object):
                     cur_node.twards.hwards = cur_node.hwards
                     break
                 else:
-                    if cur_node.twards == None:
+                    if cur_node.twards is None:
                         raise LookupError
                     else:
                         cur_node = cur_node.twards
 
 
-
-
 class Node(object):
-    """docstring for Node"""
+    u"""Create an object that represents a node in a doubly linked list."""
     def __init__(self, value, hwards=None, twards=None):
         self.hwards = hwards
         self.twards = twards
