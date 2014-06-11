6811133ea446677fe45864fc6e62e9c8f9df9339
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-11 13:38:04 -0700

    Expanded on Heap Class

--- heap/heap.py
+++ heap/heap.py
@@ -1,25 +1,31 @@
 class Heap(object):
-    def __init__(self):
-        u"""Initializes with a list that contains """
-        self.core = [u'_TOP_']
+    def __init__(self, invert=False):
+        u"""Initializes a max binary heap by default.
+
+        If invert is set to True, then it creates a min heap."""
+
+        self.core = []
+        self.invert = invert
 
     def __len__(self):
         return len(self.core)
 
     def __getitem__(self,key):
-        return self.core[key+1]
+        return self.core[key]
 
-    def _flip_parent_child(self,child_index, invert=False):
+    def _flip_parent_child(self,child_index):
         u"""Flips parent/child if child is greater than parent.
 
-        Unless invert is set to True, in which case it does the opposite.
+        Unless self.invert is set to True, in which case it does the opposite.
         Returns True if switch occured, False otherwise."""
         return bool
 
     def _get_parent(self,child_index):
+        u"""Computes the parent index from a child index."""
         return (child_index-1)//2
 
     def _get_children(self,parent_index):
+        u"""Returns a tuple consisting of the two children of a given parent."""
         return (2*parent_index+1),(2*parent_index+2)
 
     def push(self,num):
