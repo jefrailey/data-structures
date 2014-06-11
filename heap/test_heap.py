847ef36352edbaf625f4fbfee6b9a69fde7db2fb
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-11 12:57:36 -0700

    added more tests

--- heap/test_heap.py
+++ heap/test_heap.py
@@ -7,16 +7,31 @@ def _build_a_heap():
     return heap
 
 def test_init():
-    u"""Assert the initiation of heap object"""
+    u"""Assert the succesful initiation of heap object"""
     heap =_build_a_heap()
     assert isinstance(heap, Heap())
 
 def test_push():
-    heap =_build_a_heap()
-    assert len(heap) == len(sample_numbers)
-    assert heap[0] == max(sample_numbers)
+    u"""Assert that the heap has correct head through multiple pushes"""
+    heap = Heap()
+    numsPushed=[]
+    for num in sample_numbers:
+        head.push(num)
+        numsPushed.append(num)
+        assert heap[0] == max(numsPushed)
 
-def test_pop():
+def test_pop_removes_head():
+    """Assert that pop removes the head."""
     heap =_build_a_heap()
     for num in sample_numbers:
-        assert heap.pop() = heap[0]
+        assert heap.pop() == heap[0]
+    assert head.pop() == None
+
+def test_pop_reorganizes():
+    """Assert that pop properly reorganizes."""
+    heap =_build_a_heap()
+    nums_in_heap = sample_numbers[:]
+    for num in sample_numbers:
+        removed = head.pop()
+        assert removed == max(nums_in_heap)
+        nums_in_heap.remove(removed)
