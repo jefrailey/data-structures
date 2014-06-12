21d19a00a77b39714f5b6c77c71600758e1d628e
jefrailey <jcaetof@gmail.com>
2014-06-12 09:21:06 -0700

    Add print statements to test_pop_reorganizes to see issues.

--- heap/test_heap.py
+++ heap/test_heap.py
@@ -1,4 +1,5 @@
 from heap import Heap
+import random
 
 sample_numbers = [
     2, 4, 3, 7, 8, 53,
@@ -12,8 +13,8 @@ sample_numbers = [
 
 def _build_a_heap():
     heap = Heap()
-    for num in sample_numbers:
-        heap.push(num)
+    while len(heap) < len(sample_numbers):
+        heap.push(random.choice(sample_numbers))
     return heap
 
 
@@ -46,13 +47,15 @@ def test_pop_removes_head():
 def test_pop_reorganizes():
     u"""Assert that pop properly reorganizes."""
     heap = _build_a_heap()
-    nums_in_heap = sample_numbers[:]
+    nums_in_heap = heap[:]
     nums_in_heap.sort()
     nums_in_heap = nums_in_heap[::-1]
     for i, num in enumerate(sample_numbers):
+        print u"Heap: {}".format(heap[:])
+        print u"Nums_in_heap: {}".format(nums_in_heap[:])
         removed = heap.pop()
+        print "Removed: {}\n nums_in_heap[i]: {}".format(removed, nums_in_heap[i])
         assert removed == nums_in_heap[i]
-
 
 
         #AFRAID that tests might not be valid, last shift to left when popping a value COULD
