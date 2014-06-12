0ec2941590c2afed164b8be74bad37b9a9ba8e72
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-12 16:19:42 -0700

    fixed extra line issue

--- priority_queue/priority_queue.py
+++ priority_queue/priority_queue.py
@@ -42,25 +42,3 @@ class PriorityQ(Heap):
         for val in self.core:
             retval.append("{}".format(val))
         return ", ".join(retval)
-        # return retval
-
-
-"""
-     __cmp__ should return a negative integer if self < other, zero if self == other, and 
-     positive if self > other. It's usually best to define each comparison you need rather 
-     than define them all at once, but __cmp__ can be a good way to save repetition and improve
-      clarity when you need all comparisons implemented with similar criteria.
-__eq__(self, other)
-
-Defines behavior for the equality operator, ==.
-__ne__(self, other)
-Defines behavior for the inequality operator, !=.
-__lt__(self, other)
-Defines behavior for the less-than operator, <.
-__gt__(self, other)
-Defines behavior for the greater-than operator, >.
-__le__(self, other)
-Defines behavior for the less-than-or-equal-to operator, <=.
-__ge__(self, other)
-Defines behavior for the greater-than-or-equal-to operator, >=.
-"""
