25f0b7f0074f3279a54da61fd963ee6015c71879
jefrailey <jcaetof@gmail.com>
2014-06-16 18:04:49 -0700

    Add tests for initalizing nodes, adding nodes to a graph, and adding edges to a graph

--- simple_graph/test_simple_graph.py
+++ simple_graph/test_simple_graph.py
@@ -1,4 +1,6 @@
 from simple_graph import Graph
+from simple_graph import Node
+from simple_graph import Edge
 
 
 def test_nodes_empty():
@@ -15,3 +17,38 @@ def test_nodes():
 def test_edges_empty():
     g = Graph()
     assert g.edges() == []
+
+
+def test_node_init():
+    n = Node("A")
+    assert n.name == "A"
+
+
+def test_add_node():
+    g = Graph()
+    g.add_node("A")
+    assert g.nodes()[0].name == u"A"
+
+
+def test_add_edge():
+    g = Graph()
+    g.add_node(u"A")
+    g.add_node(u"B")
+    g.add_edge(u"A", u"B")
+    assert g.edges()[0].name == (u"A", u"B")
+
+
+def test_add_edge_one_new_node():
+    g = Graph()
+    g.add_node(u"A")
+    g.add_edge(u"A", u"C")
+    assert u"C" in g.nodes()
+    assert g.edges()[0].name == (u"A", u"C")
+
+
+def test_add_edge_two_new_nodes():
+    g = Graph()
+    g.add_edge(u"A", u"C")
+    assert u"A" in g.nodes()
+    assert u"C" in g.nodes()
+    assert g.edges()[0].name == (u"A", u"C")
