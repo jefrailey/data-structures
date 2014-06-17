b8f83261ea1c77f761e4a21888c7aa34434a41cf
jefrailey <jcaetof@gmail.com>
2014-06-16 17:28:34 -0700

    Update tests to account for the new initalization of Graphs

--- simple_graph/test_simple_graph.py
+++ simple_graph/test_simple_graph.py
@@ -1,12 +1,13 @@
-from simple_graph import Graph as g
+from simple_graph import Graph
 import pytest
 
 
 def test_nodes_empty():
-    with pytest.raises(AttributeError):
-        nodes = g.nodes()
+    g = Graph()
+    assert g.nodes() == []
 
 
 def test_nodes():
+    g = Graph()
     nodes = g.nodes()
     pass
