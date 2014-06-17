4975b673219a1795ab585ab6fe878dbfed60924b
jefrailey <jcaetof@gmail.com>
2014-06-16 19:16:22 -0700

    Add Graph.adjacent() to determine if two nodes are adjacent; add Graph.neighbors to show a node's neighbors

--- simple_graph/simple_graph.py
+++ simple_graph/simple_graph.py
@@ -25,7 +25,7 @@ class Graph(object):
         if node in self.nodes():
             self._nodes.remove(node)
             edges = self.edges().copy()
-            for edge in edges:  # Del while iterating? Or does the return val make thisokay?
+            for edge in edges:  # Can this be done without copying the set?
                 if node in edge:
                     self._edges.remove(edge)
         else:
@@ -39,3 +39,26 @@ class Graph(object):
 
     def has_node(self, node):
         return node in self.nodes()
+
+    def adjacent(self, node1, node2):
+        if node1 not in self.nodes() or node2 not in self.nodes():
+            raise KeyError
+            print "Passed if"
+        elif (node1, node2) and (node2, node1) not in self.edges():
+            return False
+        elif (node1, node2) or (node2, node1) in self.edges():
+            return True
+        else:
+            return "WTF"
+
+    def neighbors(self, node):
+        if node not in self.nodes():
+            raise KeyError
+        else:
+            retlist = []
+            for node1, node2 in self.edges():
+                if node1 == node:
+                    retlist.append(node2)
+                elif node2 == node:
+                    retlist.append(node1)
+        return retlist
