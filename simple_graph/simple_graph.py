fb6eebd63de3d79204788b6b1acd0bd6b7b707d8
jefrailey <jcaetof@gmail.com>
2014-06-16 18:24:54 -0700

    Revise (yet again) the internal representation of nodes and edges--they're strings/tuples in a set

--- simple_graph/simple_graph.py
+++ simple_graph/simple_graph.py
@@ -1,8 +1,8 @@
 class Graph(object):
     """docstring for Graph"""
     def __init__(self):
-        self._nodes = []
-        self._edges = []
+        self._nodes = set()
+        self._edges = set()
 
     def nodes(self):
         return self._nodes
@@ -11,22 +11,29 @@ class Graph(object):
         return self._edges
 
     def add_node(self, name):
-        self._nodes.append(Node(name))
+        self._nodes.add(name)
+
+    def add_edge(self, node1, node2):
+        if node1 not in self.nodes():
+            self.add_node(node1)
+        if node2 not in self.nodes():
+            self.add_node(node2)
+        if (node1, node2) and (node2, node1) not in self.edges():
+            self._edges.add((node1, node2))
 
 
+# class Node(object):
+#     """docstring for Node"""
+#     def __init__(self, name):
+#         super(Node, self).__init__()
+#         self.name = name
 
-class Node(object):
-    """docstring for Node"""
-    def __init__(self, name):
-        super(Node, self).__init__()
-        self.name = name
 
-
-class Edge(object):
-    """docstring for Edge"""
-    def __init__(self, name):
-        super(Edge, self).__init__()
-        self.name = name
+# class Edge(object):
+#     """docstring for Edge"""
+#     def __init__(self, name):
+#         super(Edge, self).__init__()
+#         self.name = name
 
 
 """
