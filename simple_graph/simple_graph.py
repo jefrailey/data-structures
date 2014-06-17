629e9e2cc0efff7f52346bafaae0c910a8ded64b
jefrailey <jcaetof@gmail.com>
2014-06-16 17:28:08 -0700

    Revise Graph() to initialize with emtpy lists to contain nodes and edges instead of setting them to None

--- simple_graph/simple_graph.py
+++ simple_graph/simple_graph.py
@@ -1,5 +1,17 @@
 class Graph(object):
     """docstring for Graph"""
     def __init__(self):
-        self.nodes = None
-        self.edges = None
+        self._nodes = []
+        self._edges = []
+
+    def nodes(self):
+        return self._nodes
+
+    def edges(self):
+        return self._edges
+
+
+"""
+Graph contains nodes
+Nodes contain edges
+"""
