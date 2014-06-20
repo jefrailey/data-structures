c3ca2db059f8cfbeec6a8610fb96e8c4dacda641
jefrailey <jcaetof@gmail.com>
2014-06-20 14:42:04 -0700

    Add TraversableGraph.depth_first_traversal() to progress through connected nodes in a depth first manner

--- traversable_graph/traversable_graph.py
+++ traversable_graph/traversable_graph.py
@@ -6,3 +6,23 @@ class TraversableGraph(Graph):
     """docstring for TraversableGraph"""
     def __init__(self):
         super(TraversableGraph, self).__init__()
+
+    def depth_first_traversal(self, start):
+        if start not in self.nodes():
+            raise KeyError
+        node = start
+        stack = Stack()
+        stack.push(node)
+        traversed = []
+        while len(traversed) < len(self.nodes()):
+            try:
+                node = stack.pop()
+                print node
+                traversed.insert(0, node)
+                children = self.neighbors(node)
+                for child in children:
+                    if child not in traversed:
+                       stack.push(child)
+            except LookupError:
+                break
+        return traversed
