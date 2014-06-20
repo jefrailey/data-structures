7e26654b8123d94aeff8a98185856d1581ce79de
jefrailey <jcaetof@gmail.com>
2014-06-20 14:44:34 -0700

    Add name==main block demonstrating traversals of some graphs

--- traversable_graph/traversable_graph.py
+++ traversable_graph/traversable_graph.py
@@ -46,3 +46,30 @@ class TraversableGraph(Graph):
             except LookupError:
                 break
         return traversed
+
+if __name__ == "__main__":
+    g = TraversableGraph()
+    g.add_edge(u"A", u"C")
+    g.add_edge(u"A", u"B")
+    g.add_edge(u"A", u"D")
+    g.add_edge(u"D", u"E")
+
+    circle = TraversableGraph()
+    circle.add_edge(u"A", u"B")
+    circle.add_edge(u"B", u"C")
+    circle.add_edge(u"C", u"D")
+    circle.add_edge(u"D", u"E")
+
+    d = TraversableGraph()
+    d.add_edge(u"Head", u"A")
+    d.add_edge(u"Head", 1)
+    d.add_edge(1, 2)
+    d.add_edge(2, 3)
+    d.add_edge(u"A", u"B")
+    d.add_edge(u"B", u"C")
+
+    just_nodes = TraversableGraph()
+    just_nodes.add_node(u"A")
+    just_nodes.add_node(u"B")
+    just_nodes.add_node(u"C")
+    just_nodes.add_node(u"D")
