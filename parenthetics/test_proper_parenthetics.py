caf9831d0d9299a779b834e5d5d87be8868a1d0e
jefrailey <jcaetof@gmail.com>
2014-06-30 14:24:28 -0700

    Update assert values in test_proper_parenthetics.py to match the assignment specification

--- parenthetics/test_proper_parenthetics.py
+++ parenthetics/test_proper_parenthetics.py
@@ -1,16 +1,14 @@
 import proper_parenthetics as pp
 
-answer = u"All the right pieces in all the wrong places."
-
 
 def test_open():
     a_string = u"(hello( my good friend)"
-    assert pp.parentheticals(a_string) == -1
+    assert pp.parentheticals(a_string) == 1
 
 
 def test_closed():
     a_string = u"(hello( my good friend)))"
-    assert pp.parentheticals(a_string) == 1
+    assert pp.parentheticals(a_string) == -1
 
 
 def test_balanced_proper():
@@ -20,19 +18,19 @@ def test_balanced_proper():
 
 def test_balanced_improper1():
     a_string = u")helo (hello( my good friend)"
-    assert pp.parentheticals(a_string) == answer
+    assert pp.parentheticals(a_string) == -2
 
 
 def test_balanced_improper2():
     a_string = u")()()()helo (hello( my go()()()()od friend)"
-    assert pp.parentheticals(a_string) == answer
+    assert pp.parentheticals(a_string) == -2
 
 
 def test_balanced_improper3():
     a_string = u")helo (hello( my good friend)()()()()"
-    assert pp.parentheticals(a_string) == answer
+    assert pp.parentheticals(a_string) == -2
 
 
 def test_balanced_improper4():
     a_string = u"())(((((((((())))))))))("
-    assert pp.parentheticals(a_string) == answer
+    assert pp.parentheticals(a_string) == -2
