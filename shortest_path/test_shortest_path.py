d84bb4e0bae1d9ef6e8a0cf33b4a3a3d84cb9fd9
jefrailey <jcaetof@gmail.com>
2014-07-01 15:44:23 -0700

    Add test_shortest_path_triangle_two_edge_path() to assert dijkstra() determines the shortest path even if it takes multiple edges

--- shortest_path/test_shortest_path.py
+++ shortest_path/test_shortest_path.py
@@ -2,7 +2,7 @@ from weighted_graph import WeightedGraph
 from shortest_path import dijkstra
 
 
-def test_shortest_path_simple():
+def test_shortest_path_triangle_one_edge_path():
     wg = WeightedGraph()
     wg.add_edge('S', 'A', 1)
     wg.add_edge('A', 'E', 3)
@@ -10,3 +10,13 @@ def test_shortest_path_simple():
     path, distance = dijkstra(wg, 'S', 'E')
     assert path == ['S', 'E']
     assert distance == 3
+
+
+def test_shortest_path_triangle_two_edge_path():
+    wg = WeightedGraph()
+    wg.add_edge('S', 'A', 1)
+    wg.add_edge('A', 'E', 1)
+    wg.add_edge('S', 'E', 3)
+    path, distance = dijkstra(wg, 'S', 'E')
+    assert path == ['S', 'A', 'E']
+    assert distance == 2
