dfa52f59b48b0efbc42134d8f5d2789c89e60c12
jefrailey <jcaetof@gmail.com>
2014-07-02 14:49:27 -0700

    Add Bst.size() to return number of nodes in tree; revise Bst.insert() to remove None and inf entries in Bst._nodes

--- bst/bst.py
+++ bst/bst.py
@@ -3,15 +3,44 @@ class Bst(object):
     def __init__(self):
         super(Bst, self).__init__()
         self._nodes = {}  # nodes[node] = [Left, Right, Parent]
-
-    def insert(self, val):
-        pass
+        self._root = None
 
     def contains(self, val):
         return bool(self._nodes.get(val, False))
 
+    def insert(self, val):
+        if not self.contains(val):
+            if self._root is None:
+                self._nodes[val], self._root = [None, float('inf'), None], val
+            else:
+                node = self._root
+                while True:
+                    left, right, parent = self._nodes[node]
+                    if (left < val) and (val < node):
+                        try:
+                            self._nodes[left][2] = val
+                        except KeyError:
+                            pass
+                        self._nodes[node][0] = val
+                        self._nodes[val] = [left, float('inf'), node]
+                        break
+                    elif (right > val) and (val > node):
+                        try:
+                            self._nodes[right][2] = val
+                        except KeyError:
+                            pass
+                        self._nodes[node][1] = val
+                        self._nodes[val] = [None, right, node]
+                        break
+                    elif val < left:
+                        node = self._nodes[left]
+                        print node
+                    elif val > right:
+                        node = self._nodes[right]
+                        print node
+
     def size(self):
-        pass
+        return len (self._nodes.keys())
 
     def depth(self):
         pass
