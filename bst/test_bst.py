a18b12dafd6770c27c13b949acf21bdce79d4d95
jefrailey <jcaetof@gmail.com>
2014-07-02 12:59:49 -0700

    Add test for initialization of Bst._nodes

--- bst/test_bst.py
+++ bst/test_bst.py
@@ -1,8 +1,14 @@
 from bst import Bst
 
 
-def test_initalize():
-    raise AssertionError
+def test_initalize_is_Bst():
+    bst = Bst()
+    assert isinstance(bst, Bst)
+
+
+def test_initialize_empty():
+    bst = Bst()
+    assert bst._nodes == {}
 
 
 def test_insert():
