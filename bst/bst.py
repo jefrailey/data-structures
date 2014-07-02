5e6132480756b23c0a12185a1fd180fbaedfd0b9
jefrailey <jcaetof@gmail.com>
2014-07-02 12:59:22 -0700

    Update Bst() init to create a dictionary to store nodes

--- bst/bst.py
+++ bst/bst.py
@@ -2,6 +2,7 @@ class Bst(object):
     """docstring for Bst"""
     def __init__(self):
         super(Bst, self).__init__()
+        self._nodes = {}  # nodes[node] = [Left, Right, Parent]
 
     def insert(self, val):
         pass
