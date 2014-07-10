396623ba882a763b151ae7b2ca402750be157967
jefrailey <jcaetof@gmail.com>
2014-07-10 14:22:17 -0700

    Rewrite _make_wordlist() to open the words file via a relative path

--- hash_table/test_hash_table.py
+++ hash_table/test_hash_table.py
@@ -2,7 +2,7 @@ from hash_table import HashTable
 
 
 def _make_wordlist():
-    with open('/usr/share/dict/words', 'r') as f:
+    with open('words', 'r') as f:
         wordlist = []
         for word in f.readlines():
             wordlist.append(word)
