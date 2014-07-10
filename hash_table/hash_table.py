da9770d0c1a6fa49c17538ffaa4de75d244ba7c1
jefrailey <jcaetof@gmail.com>
2014-07-10 13:19:51 -0700

    Add HashTable.hash() to convert a key (string) into a int that represents an index position in the 'hash list'

--- hash_table/hash_table.py
+++ hash_table/hash_table.py
@@ -2,9 +2,17 @@ class HashTable(object):
     """docstring for HashTable"""
     def __init__(self, size):
         self.size = size
+        self.table = [[] for i in xrange(self.size)]
 
     def hash(self, key):
-        pass
+        u"""Return hashed key."""
+        if not isinstance(key, str):
+            raise KeyError
+        key_sum = 0
+        for letter in key:
+            key_sum += ord(letter)
+        mask = self.size - 1
+        return key_sum & mask
 
     def set(self, key, val):
         pass
