26498aebf381e1ab1f66cb085a8ab3bfdc41936b
jefrailey <jcaetof@gmail.com>
2014-07-10 12:35:23 -0700

    Add hash_table.py with HashTable() skeleton

---
+++ hash_table/hash_table.py
@@ -0,0 +1,13 @@
+class HashTable(object):
+    """docstring for HashTable"""
+    def __init__(self, size):
+        self.size = size
+
+    def hash(self, key):
+        pass
+
+    def set(self, key, val):
+        pass
+
+    def get(self, key):
+        pass
