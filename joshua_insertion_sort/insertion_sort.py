9b87138adb6132b1986d7047aa5e85a758830220
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-07-16 14:29:13 -0700

    wrote first timing test

--- joshua_insertion_sort/insertion_sort.py
+++ joshua_insertion_sort/insertion_sort.py
@@ -6,3 +6,15 @@ def insertion_sort(unsorted):
             i -= 1
         unsorted[i] = val
     return unsorted
+
+
+if __name__ == '__main__':
+    from functools import partial
+    import timeit
+    reps = 10000
+    length = 1000
+    print "\nAll tests run with " + str(reps) + " repetitions.\n"
+    unsorted_list = [1,5,3,8,6,2,0,4,7,9]
+    t= timeit.Timer(partial(insertion_sort, unsorted_list))
+    print "A list of length ten takes: " + str(t.timeit(reps)) + " seconds to sort.\n"
+
