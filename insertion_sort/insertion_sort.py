c889ca178c1b27d97607f7b064f7c9884c734aa1
jefrailey <jcaetof@gmail.com>
2014-07-16 13:53:11 -0700

    Rename joshua_insertion_sort/ to insertion_sort/

---
+++ insertion_sort/insertion_sort.py
@@ -0,0 +1,8 @@
+def insertion_sort(unsorted):
+    u"""Return list sorted via insertion sort."""
+    for i, val in enumerate(unsorted):
+        while val < unsorted[i-1] and i > 0:
+            unsorted[i] = unsorted[i-1]
+            i -= 1
+        unsorted[i] = val
+    return unsorted
