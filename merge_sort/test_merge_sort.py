393493af403e4fb6d677e808fda9d3130329bd70
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-07-16 14:46:54 -0700

    updated tests for merge_sort

--- merge_sort/test_merge_sort.py
+++ merge_sort/test_merge_sort.py
@@ -1,18 +1,18 @@
-from insertion_sort import insertion_sort
+from merge_sort import merge_sort
 
 def test_short_sort():
     unsorted_list = [1,5,3,8,6,2,0,4,7,9]
-    sorted_list = insertion_sort(unsorted_list)
+    sorted_list = merge_sort(unsorted_list)
     assert sorted_list == range(10)
 
 
 def test_already_sorted_sort():
     unsorted_list = range(1000)
-    sorted_list = insertion_sort(unsorted_list)
+    sorted_list = merge_sort(unsorted_list)
     assert sorted_list == range(1000)
 
 def test_backwards_sorted_sort():
     unsorted_list = range(1000)
     unsorted_list.reverse()
-    sorted_list = insertion_sort(unsorted_list)
+    sorted_list = merge_sort(unsorted_list)
     assert sorted_list == range(1000)
