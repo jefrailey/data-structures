44a3466ac41ef91176d25badc00723d0e655839e
jefrailey <jcaetof@gmail.com>
2014-07-17 15:47:11 -0700

    Add test_quick_sort.py to assert that quick_sort() returns a sorted list

---
+++ tests/test_quick_sort.py
@@ -0,0 +1,20 @@
+from data_structures.lists.quick_sort import quick_sort
+
+
+def test_short_sort():
+    unsorted_list = [1, 5, 3, 8, 6, 2, 0, 4, 7, 9]
+    sorted_list = quick_sort(unsorted_list)
+    assert sorted_list == range(10)
+
+
+def test_already_sorted_sort():
+    unsorted_list = range(1000)
+    sorted_list = quick_sort(unsorted_list)
+    assert sorted_list == range(1000)
+
+
+def test_backwards_sorted_sort():
+    unsorted_list = range(1000)
+    unsorted_list.reverse()
+    sorted_list = quick_sort(unsorted_list)
+    assert sorted_list == range(1000)
