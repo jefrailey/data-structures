28fc425cb6ba84527f49926defd02f8bfda22f1d
jefrailey <jcaetof@gmail.com>
2014-07-17 15:06:24 -0700

    Add quick_sort.py with quick_sort() and _median() to sort a list via the quick sort algorithm

---
+++ data_structures/lists/quick_sort.py
@@ -0,0 +1,21 @@
+def quick_sort(list_):
+    u"""Return a list sorted by the quick sort algorithm."""
+    if list_:
+        pivot = _median(list_[0], list_[len(list_)//2], list_[-1])
+
+        left = quick_sort([x for x in list_[1:] if x < pivot])
+        right = quick_sort([x for x in list_[1:] if x >= pivot])
+
+        return left + [pivot] + right
+    else:
+        return []
+
+
+def _median(a, b, c):
+    u"""Return the median of three values."""
+    if (a <= b <= c) or (c <= b <= a):
+        return b
+    elif (a <= c <= b) or (b <= c <= a):
+        return c
+    else:
+        return a
