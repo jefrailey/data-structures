a5b6bc0e965d652609894266e7c9678f7b3ce065
jefrailey <jcaetof@gmail.com>
2014-07-17 17:38:58 -0700

    Add radix_sort.py with radix_sort() to sort a list of ints via the radix_sort algorithm

--- data_structures/lists/radix_sort.py
+++ data_structures/lists/radix_sort.py
@@ -1,19 +1,18 @@
 def radix_sort(list_):
-    """Return a sorted list by the radix sort algorithm."""
-    bins={}
+    u"""Return a sorted list by the radix sort algorithm."""
     depth = 0
     while True:
+        bins = {}
         depth += 1
         for num in list_:
-            bin_key = ((num % (10 ** depth))-(num % (10 ** (depth-1))))/10
-            bins.setdefault(bin_key, [num]).append(num)
-        # list_ = [x for bins[x] in [l for l in sorted(bins.keys())]]
+            dc = 10 ** (depth-1)
+            bin_key = ((num % (10 ** depth))-(num % dc))/dc
+            bins.setdefault(bin_key, []).append(num)
         list_ = []
         for bin_ in sorted(bins.keys()):
             for val in bins[bin_]:
                 list_.append(val)
 
-
-        if len(bins[bin_key])==len(list_):
+        if len(bins[bin_key]) == len(list_) and depth > 0:
             break
     return list_
