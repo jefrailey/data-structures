d4df0d0f67be9962dacb2d2ed440f43239de0233
jefrailey <jcaetof@gmail.com>
2014-09-11 12:54:30 -0700

    Update test_doubly_linked.py to account for changes made to attribute names in DoublyLinked() and Node()

--- tests/test_doubly_linked.py
+++ tests/test_doubly_linked.py
@@ -2,12 +2,12 @@ from data_structures.lists.doubly_linked import DoublyLinked, Node
 import pytest
 import random
 
-values = [u'a', 1, True, None, False, 0]
+VALUES = [u'a', 1, True, None, False, 0]
 
 
 def test_Node_init():
     u"""Assert a Node instantiates with a given value."""
-    for val in values:
+    for val in VALUES:
         node = Node(val)
         assert isinstance(node, Node)
         assert node.value == val
@@ -22,27 +22,27 @@ def test_DoublyLinked_init():
 def test_insert():
     u"""Assert .insert() adds value/node to the h of the doubly linked list."""
     doubly_linked = DoublyLinked()
-    for val in values:
+    for val in VALUES:
         doubly_linked.insert(val)
-        assert doubly_linked.h.value == val
-    assert doubly_linked.h.value == values[-1]
+        assert doubly_linked.head.value == val
+    assert doubly_linked.head.value == VALUES[-1]
 
 
 def test_append():
     u"""Assert .append() adds a value to the t of the doubly linked list."""
     doubly_linked = DoublyLinked()
-    for val in values:
+    for val in VALUES:
         doubly_linked.append(val)
-        assert doubly_linked.t.value == val
-    assert doubly_linked.t.value == values[-1]
+        assert doubly_linked.tail.value == val
+    assert doubly_linked.tail.value == VALUES[-1]
 
 
 def test_pop():
     u"""Assert .pop() removes and returns the h value from the list."""
     doubly_linked = DoublyLinked()
-    for val in values:
+    for val in VALUES:
         doubly_linked.append(val)
-    for val in values:
+    for val in VALUES:
         assert doubly_linked.pop() == val
     assert doubly_linked.pop() is None
 
@@ -50,9 +50,9 @@ def test_pop():
 def test_shift():
     u"""Assert .shift() removes and returns the t value from the list."""
     doubly_linked = DoublyLinked()
-    for val in values:
+    for val in VALUES:
         doubly_linked.insert(val)
-    for val in values:
+    for val in VALUES:
         assert doubly_linked.shift() == val
     assert doubly_linked.shift() is None
 
@@ -60,16 +60,16 @@ def test_shift():
 def test_remove():
     u"""Assert .remove() removes the first matching value from the list."""
     doubly_linked = DoublyLinked()
-    for val in values:
+    for val in VALUES:
         doubly_linked.insert(val)
-    test_vals = values[:]
+    test_vals = VALUES[:]
     while test_vals:
         val = random.choice(test_vals)
         doubly_linked.remove(val)
         test_vals.remove(val)
     assert doubly_linked.pop() is None
-    assert doubly_linked.h is None
-    assert doubly_linked.t is None
+    assert doubly_linked.head is None
+    assert doubly_linked.tail is None
     doubly_linked.insert(1)
     with pytest.raises(LookupError):
         doubly_linked.remove("test value")
