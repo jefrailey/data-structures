857274ca6d37aa3c96a8ef1e500109a9a36cbdf8
jefrailey <jcaetof@gmail.com>
2014-09-13 11:48:42 -0700

    Refactor Heap() to improve readability & pythonicity
    Method names are more explicit, class docstring spaced in accordance with PEP 257, unused method removed

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -1,60 +1,50 @@
 class Heap(object):
-    def __init__(self, invert=False):
-        u"""Initializes a max binary heap by default.
 
-        If invert is set to True, then it creates a min heap."""
+    u"""An object that represents a max binary heap."""
 
+    def __init__(self):
         self.core = []
-        self.invert = invert
-
-    def __len__(self):
-        return len(self.core)
-
-    def __getitem__(self, key):
-        return self.core[key]
-
-    def _flip_p_c(self, child_index):
-        u"""Flips p/c if c is greater than p.
-
-        Unless self.invert is set to True, in which case it does the opposite.
-        Returns True if switch occured, False otherwise."""
-        child = self.core[child_index]
-        the_parent_index = self._get_p(child_index)
-        parent = self.core[the_parent_index]
-        if child > parent:
-            self.core[child_index], self.core[the_parent_index] = parent, child
-            return True
-        else:
-            return False
-
-    def _get_p(self, child_index):
-        u"""Computes the p index from a c index."""
-        if child_index:
-            return (child_index-1)//2
-        else:
-            return 0
-
-    def _get_cren(self, p_index):
-        u"""Returns a tuple consisting of the two cren of a given p."""
-        return (2*p_index+1), (2*p_index+2)
-
-    def _get_sib(self, index):
-        parent = self._get_p(index)
-        sib = self._get_cren(parent)
-        for i in sib:
-            if i != index:
-                return i
 
     def push(self, num):
-        u"""Adds a vaue to the heap and correctly reorganizes."""
+        u"""Add a vaue to the heap and reorganize the heap."""
         self.core.append(num)
         self._organize_up()
 
+    def pop(self):
+        u"""Remove the top value from the heap and reorganize the heap."""
+        try:
+            popped_value = self.core[0]
+        except IndexError:
+            return None
+        else:
+            self._organize_down()
+            return popped_value
+
+    def _flip_parent_child(self, child_index):
+        u"""Swap parent with child if child is greater; return False if not."""
+        child = self.core[child_index]
+        parent_index = self._get_parent(child_index)
+        parent = self.core[parent_index]
+        if child > parent:
+            self.core[child_index], self.core[parent_index] = parent, child
+            return True
+        return False
+
+    def _get_parent(self, child_index):
+        u"""Compute the parent index from a child index."""
+        if child_index:
+            return (child_index-1)//2
+        return 0
+
+    def _get_children(self, p_index):
+        u"""Return a tuple consisting of the two children of a given p."""
+        return (2*p_index+1), (2*p_index+2)
+
     def _organize_up(self, num_index=None):
-        while num_index is None:
+        if num_index is None:
             num_index = len(self.core)-1
-        while self._flip_p_c(num_index):
-            num_index = self._get_p(num_index)
+        while self._flip_parent_child(num_index):
+            num_index = self._get_parent(num_index)
 
     def _organize_down(self):
         num_index = 0
@@ -68,9 +58,8 @@ class Heap(object):
 
         This process continues until the popped value reaches the bottom,
         where it is ready to be removed."""
-
         current = self.core[current_index]
-        left_index, right_index = self._get_cren(current_index)
+        left_index, right_index = self._get_children(current_index)
 
         # If the current has no children:
         if left_index >= len(self.core) and right_index >= len(self.core):
@@ -95,12 +84,8 @@ class Heap(object):
                 self.core[right_index], self.core[current_index] = current, right
                 return right_index, False
 
-    def pop(self):
-        u"""Removes the top value from the heap and correctly reorganizes."""
-        try:
-            popped_value = self.core[0]
-        except IndexError:
-            return None
-        else:
-            self._organize_down()
-            return popped_value
+    def __len__(self):
+        return len(self.core)
+
+    def __getitem__(self, key):
+        return self.core[key]
