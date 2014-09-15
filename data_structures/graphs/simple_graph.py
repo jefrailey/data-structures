fb03f7051eaf967cdb4d6001fe004a8ccdf34184
jefrailey <jcaetof@gmail.com>
2014-09-15 09:08:32 -0700

    Improve pythonicity and readability of Graph()
    Add docstrings to all public methods. Change Graph.nodes() and Graph.edges() to return a list that copies the related attributes rather than returning the attributes themselves to prevent manipulation of the nodes and edges outside of the add_ and del_ methods

--- data_structures/graphs/simple_graph.py
+++ data_structures/graphs/simple_graph.py
@@ -1,19 +1,25 @@
 class Graph(object):
-    """docstring for Graph"""
+
+    u"""A simple undirected graph."""
+
     def __init__(self):
         self._nodes = set()
         self._edges = set()
 
     def nodes(self):
-        return self._nodes
+        u"""Return a list of nodes in the graph."""
+        return list(self._nodes)
 
     def edges(self):
-        return self._edges
+        u"""Return a list of edges in the graph."""
+        return list(self._edges)
 
     def add_node(self, name):
+        u"""Add a node to the graph."""
         self._nodes.add(name)
 
     def add_edge(self, node1, node2):
+        u"""Add an edge to the graph and corresponding nodes."""
         if node1 not in self.nodes():
             self.add_node(node1)
         if node2 not in self.nodes():
@@ -22,9 +28,10 @@ class Graph(object):
             self._edges.add((node1, node2))
 
     def del_node(self, node):
+        u"""Remove node and corresponding edges from the graph."""
         if node in self.nodes():
             self._nodes.remove(node)
-            edges = self.edges().copy()
+            edges = self.edges()
             for edge in edges:  # Can this be done without copying the set?
                 if node in edge:
                     self._edges.remove(edge)
@@ -32,24 +39,28 @@ class Graph(object):
             raise ValueError(u"Node not found")
 
     def del_edge(self, node1, node2):
+        u"""Remove edge between node1 and node2 from the graph."""
         try:
             self._edges.remove((node1, node2))
         except KeyError:
             self._edges.remove((node2, node1))
 
     def has_node(self, node):
+        u"""Return True if node is in the graph; False if not."""
         return node in self.nodes()
 
     def adjacent(self, node1, node2):
+        u"""Return True if node1 and node2 are adjacent; False if not."""
         if node1 not in self.nodes() or node2 not in self.nodes():
             raise KeyError
-        elif (node1, node2) not in self.edges() and (node2, node1)\
-                not in self.edges():
+        elif (node1, node2) not in self.edges() and (
+                (node2, node1) not in self.edges()):
             return False
         else:
             return True
 
     def neighbors(self, node):
+        u"""Return a list of nodes adjacent to a node."""
         if node not in self.nodes():
             raise KeyError
         else:
